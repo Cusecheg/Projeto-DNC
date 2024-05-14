dce7c2fca8e6d6962741e3b83bb48e91d54f8efe
Juan Useche <juanuseche47@gmail.com>
2024-05-14 12:16:24 -0300

    apagando console.log da page principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { ControlledCarousel } from "@/components";
 import SectionHome from "@/components/section-home/page";
 import { hashSync } from "bcrypt-ts";
 
-console.log(hashSync("123456"))
 
 export default function Home() {
   return (
